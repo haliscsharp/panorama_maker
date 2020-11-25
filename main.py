f8275b474d11193ef9e616b48877618c450a7a7b
haliscsharp <haliscsharp@gmail.com>
2020-11-25 12:17:12 +0100

    Added comments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 
+# A kep fekete szeleinek levagasa
 def trim(frame):
     if not np.sum(frame[0]):
         return trim(frame[1:])
@@ -16,6 +17,7 @@ def trim(frame):
         return trim(frame[:,:-2])
     return frame
 
+# Kepek beolvasasa, atmeretezese es szurkearnyalatossa tetele
 img_ = cv2.imread('2.jpg')
 img_ = cv2.resize(img_, (0,0),None,0.4,0.4,None)
 img1 = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
@@ -24,13 +26,17 @@ img = cv2.imread('1.jpg')
 img = cv2.resize(img, (0,0),None,0.4,0.4,None)
 img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-sift = cv2.SIFT_create()
-kp1, des1 = sift.detectAndCompute(img1, None)
-kp2, des2 = sift.detectAndCompute(img2, None)
 
+# Kulcspontok kiszamolasa SIFT algoritmus segitsegevel
+sift = cv2.SIFT_create()
+keypoint1, des1 = sift.detectAndCompute(img1, None)
+keypoint2, des2 = sift.detectAndCompute(img2, None)
+
+# Kulcspontok osszehasonlitasa FLANN(Gyors Konyvtar a Legkoyelebbi Szomszedok megtalalasara) segitsegevel
 match=cv2.BFMatcher()
 matches=match.knnMatch(des1,des2,k=2)
 
+# Jo egyezesek kigyujtese es ezek abrazolasa a kepek kozott
 good=[]
 for m,n in matches:
     if m.distance<0.35*n.distance:
@@ -38,13 +44,13 @@ for m,n in matches:
 
 draw_parameters = dict(matchColor=(0,255,0), singlePointColor=None, flags=2)
 
-img3=cv2.drawMatches(img_,kp1,img,kp2,good,None,**draw_parameters)
+img3=cv2.drawMatches(img_,keypoint1,img,keypoint2,good,None,**draw_parameters)
 
-
+# A Kep homography tehat a ket kep kozos metszetenek megtalalasa
 MIN_MATCH_COUNT=10
 if len(good)>MIN_MATCH_COUNT:
-    src_pts=np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+    src_pts=np.float32([keypoint1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+    dst_pts = np.float32([keypoint2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
     M, mask = cv2.findHomography(src_pts,dst_pts, cv2.RANSAC, 5.0)
     h,w=img1.shape
@@ -55,12 +61,13 @@ if len(good)>MIN_MATCH_COUNT:
 else:
     print("Not enough matches for approppriate stitching")
 
+# A kep perspektivajanak megvaltoztatasa
 dst=cv2.warpPerspective(img_,M,(img.shape[1]+img_.shape[1], img.shape[0]))
 dst[0:img.shape[0], 0:img.shape[1]]=img
 
-
-cv2.imshow('sift_left', cv2.drawKeypoints(img_, kp1, None))
-cv2.imshow('sift_right', cv2.drawKeypoints(img, kp2, None))
+# Az egyes lepesek es a vegeredmeny megjelenitese
+cv2.imshow('sift_left', cv2.drawKeypoints(img_, keypoint1, None))
+cv2.imshow('sift_right', cv2.drawKeypoints(img, keypoint2, None))
 cv2.imshow('drawMatches',img3)
 cv2.imshow('stitched',dst)
 cv2.imshow('cropped',trim(dst))
